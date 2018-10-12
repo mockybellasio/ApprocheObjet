fde377eedb543bb4d670590ad1932851be7d92f0
mockybellasio <43879122+mockybellasio@users.noreply.github.com>
2018-10-12 16:14:46 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+package main;
+
+import entity.AdressePostale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    AdressePostale adresse1 = new AdressePostale(10, "rue des Fleurs" , 34000, "Montpellier");
+    
+    AdressePostale adresse2 = new AdressePostale(15, " rue de la gare", 34000, "Montpellier");
+    
+    
+	System.out.println(adresse1);
+	
+	System.out.println(adresse2);
+	
+	
+	
+
+	}
+
+
+	}
+
+	
+	
+
+
